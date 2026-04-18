04befafcac388b2926c0c6f1cb656d6e8e82693b
HaiNV18 <42117378+HaiNV18@users.noreply.github.com>
2026-04-18 13:25:57 +0700

    Create huong dan MongoDB

--- module3/bai1/src/main/resources/data/accounts.sql
+++ module3/bai1/src/main/resources/data/accounts.sql
@@ -13,12 +13,12 @@ CREATE TABLE accounts (
 
 INSERT INTO accounts (id, username, password, firstname, lastname, email, phone, role, id_country) VALUES
 (1, 'admin1', '1234', 'Which', 'John', 'admin1@gmail.com', '0123456789', 1, 1),
-(2, 'admin2', '1234', 'Donald', 'Trump', 'admin2@gmail.com', '0888333222', 1, 2),
-(3, 'admin3', '1234', 'Baron', 'Trump', 'admin3@gmail.com', '0888333221', 1, 3),
-(4, 'user1', '1234', 'Baron', 'Trump', 'user1@gmail.com', '0888333225', 2, 4),
-(5, 'user2', '1234', 'Baron', 'Trump', 'user2@gmail.com', '0888333275', 2, 5),
-(6, 'user3', '1234', 'Baron', 'Trump', 'user3@gmail.com', '0888333232', 2, 5),
-(7, 'user4', '1234', 'Baron', 'Trump', 'user4@gmail.com', '0888333241', 2, 5),
+(2, 'admin2', '1234', 'Oliver', 'Burke', 'admin2@gmail.com', '0888333222', 1, 2),
+(3, 'admin3', '1234', 'David', 'Preu', 'admin3@gmail.com', '0888333221', 1, 3),
+(4, 'user1', '1234', 'Rani', 'Khedira', 'user1@gmail.com', '0888333225', 2, 4),
+(5, 'user2', '1234', 'Tim', 'Skarke', 'user2@gmail.com', '0888333275', 2, 5),
+(6, 'user3', '1234', 'Leopold', 'Querfeld', 'user3@gmail.com', '0888333232', 2, 5),
+(7, 'user4', '1234', 'Tom', 'Rothe', 'user4@gmail.com', '0888333241', 2, 5),
 (8, 'user5', '1234', 'Baron', 'Trump', 'user5@gmail.com', '0888334425', 2, 8),
 (9, 'user6', '1234', 'Baron', 'Trump', 'user6@gmail.com', '0888333445', 2, 9)
 ;
