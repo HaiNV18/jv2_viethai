28fa727c795be76ed0a91af6f2966a642e6d31c7
HaiNV18 <42117378+HaiNV18@users.noreply.github.com>
2026-06-23 18:19:49 +0700

    Create CartItemDto

--- module4/ecommerce-do-an-1/src/main/java/com/myweb/ecommerce/service/BillService.java
+++ module4/ecommerce-do-an-1/src/main/java/com/myweb/ecommerce/service/BillService.java
@@ -1,14 +1,21 @@
 package com.myweb.ecommerce.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.myweb.ecommerce.dto.CartItemDto;
 import com.myweb.ecommerce.dto.request.PlaceOrderRequest;
 import com.myweb.ecommerce.model.Bill;
 import com.myweb.ecommerce.model.BillProduct;
 import com.myweb.ecommerce.repository.BillProductRepository;
 import com.myweb.ecommerce.repository.BillRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class BillService {
 
     @Autowired
@@ -17,32 +24,55 @@ public class BillService {
     @Autowired
     private BillProductRepository billProductRepository;
 
+    private final ObjectMapper objectMapper;
+
     public void save(PlaceOrderRequest req) {
+        try {
+            List<CartItemDto> cartItems = objectMapper.readValue(
+                    req.getCartData(),
+                    new TypeReference<List<CartItemDto>>() {}
+            );
+
+            // Save Bill
+            Bill bill = new Bill();
+            bill.setBillId("20260623035520");
+            bill.setFirstname(req.getFirstname());
+            bill.setLastname(req.getLastname());
+            bill.setEmail(req.getEmail());
+            bill.setPhone(req.getPhone());
+            bill.setAddress(req.getAddress());
+
+            Bill result = billRepository.save(bill);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot save order", e);
+        }
+
         // Save bill
-        Bill bill = new Bill();
-        bill.setBillId("20260620035520");
-        bill.setFirstname(req.getFirstname());
-        bill.setLastname(req.getLastname());
-        bill.setEmail(req.getEmail());
-        bill.setPhone(req.getPhone());
-        bill.setAddress(req.getAddress());
-        Bill result = billRepository.save(bill);
+//        Bill bill = new Bill();
+//        bill.setBillId("20260620035520");
+//        bill.setFirstname(req.getFirstname());
+//        bill.setLastname(req.getLastname());
+//        bill.setEmail(req.getEmail());
+//        bill.setPhone(req.getPhone());
+//        bill.setAddress(req.getAddress());
+//        Bill result = billRepository.save(bill);
 
         // Save bill_product
-        BillProduct billProduct1 = new BillProduct();
-        billProduct1.setBillId(result.getBillId());
-        billProduct1.setNameProduct("Iphone 15");
-        billProduct1.setPrice(9000.0);
-        billProduct1.setQty(1);
-        billProduct1.setTotal(9000);
-        billProductRepository.save(billProduct1);
+//        BillProduct billProduct1 = new BillProduct();
+//        billProduct1.setBillId(result.getBillId());
+//        billProduct1.setNameProduct("Iphone 15");
+//        billProduct1.setPrice(9000.0);
+//        billProduct1.setQty(1);
+//        billProduct1.setTotal(9000);
+//        billProductRepository.save(billProduct1);
 
-        BillProduct billProduct2 = new BillProduct();
-        billProduct2.setBillId(result.getBillId());
-        billProduct2.setNameProduct("Samsung S24");
-        billProduct2.setPrice(6000.0);
-        billProduct2.setQty(1);
-        billProduct2.setTotal(6000);
-        billProductRepository.save(billProduct2);
+//        BillProduct billProduct2 = new BillProduct();
+//        billProduct2.setBillId(result.getBillId());
+//        billProduct2.setNameProduct("Samsung S24");
+//        billProduct2.setPrice(6000.0);
+//        billProduct2.setQty(1);
+//        billProduct2.setTotal(6000);
+//        billProductRepository.save(billProduct2);
     }
 }
